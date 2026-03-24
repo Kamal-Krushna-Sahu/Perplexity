2a31161f1f72cd6a6e1baec423442e117cc27aec
Kamal-Krushna-Sahu <sahukamalkrushna369@gmail.com>
2026-03-24 20:15:46 +0530

    morgan logger setup

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,10 +1,12 @@
 import express from "express";
 import handleError from "./middlewares/error.middleware.js";
+import morgan from "morgan";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 
 // import router
 import authRouter from "./routes/auth.routes.js";
